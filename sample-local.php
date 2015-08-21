82feff803638484dcdf41ecef8514d67375fb92f
Suriyaa Kudo <SuriyaaKudoIsc@users.noreply.github.com>
2015-08-21 16:45:00 +0200

    Create sample-local.php
    to do!

---
+++ sample-local.php
@@ -0,0 +1,19 @@
+<?php
+
+$kgTool->setSettings(array(
+	// This must be a sandbox clone of MediaWiki, do not use your main
+	// "dev-wiki" clone because it will wipe out anything that doesn't
+	// belong in a fresh checkout.
+	// Also make sure you don't use this MediaWiki clone for other
+	// scripts (e.g. a code searcher or analyzer) because it will be
+	// swapping HEAD and active branch back and forth between very
+	// old versions of MediaWiki, new versions and even potentially
+	// unstable deveopment branches.
+	'mediawikiCoreRepoDir' => __DIR__ . '/remotes/mediawiki-core',
+
+	// This (among other temporary files) where the tarballs will be stored
+	'cacheDir' => __DIR__ . '/cache',
+
+	// Log files will be written here. Only the log of the last run is kept.
+	'logsDir' => __DIR__ . '/logs',
+));
