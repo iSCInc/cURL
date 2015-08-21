bdcbe7c4ae860b028d14102e64ce81666c9259cf
Suriyaa Kudo <SuriyaaKudoIsc@users.noreply.github.com>
2015-08-21 16:44:42 +0200

    Create common.php
    to do!

---
+++ common.php
@@ -0,0 +1,30 @@
+<?php
+
+// BaseTool & Localization
+require_once __DIR__ . '/lib/basetool/InitTool.php';
+require_once KR_TSINT_START_INC;
+
+// Class for this tool
+require_once __DIR__ . '/class.php';
+$kgTool = new KrSnapshots();
+
+// Local settings
+require_once __DIR__ . '/local.php';
+
+$I18N = new TsIntuition( 'mwsnapshots' );
+
+$toolConfig = array(
+	'displayTitle' => $I18N->msg( 'title-overview' ),
+	'remoteBasePath' => dirname( $kgConf->getRemoteBase() ). '/',
+	'revisionId' => '0.2.0',
+	'styles' => array(
+		'main.css',
+	),
+	'scripts' => array(
+		'main.js',
+	),
+	'I18N' => $I18N,
+);
+
+$kgBaseTool = BaseTool::newFromArray( $toolConfig );
+$kgBaseTool->setSourceInfoGithub( 'Krinkle', 'mw-tool-snapshots', __DIR__ );
